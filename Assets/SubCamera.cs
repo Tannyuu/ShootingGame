8f3afc473e5f6c2ac2c8b534f557250a7734ccd2
hogehoge <hogehoge@gmail.com>
2022-02-22 15:05:24 +0900

    カメラ切り替え機能追加

---
+++ Assets/SubCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubCamera : MonoBehaviour
+{
+	public Transform player;
+
+	public GameObject mainCamera;
+	public GameObject subCamera;
+
+	
+
+	void LateUpdate()
+	{
+		//transform.position = player.position + (-player.forward * 3.0f) + (player.up * 1.0f);
+		transform.LookAt(player.position);
+
+		
+	}
+}
