846842849e3d2e54d35d9ae894ede16835558c3b
cf <hoge>
2022-02-22 13:24:10 +0900

    キャラが回転して、クリックでGunから弾丸が出る

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    
+	public GameObject prefab;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			//引数一つでInstantiate
+			GameObject obj = Instantiate(prefab);
+			
+			//親要素設定（今回はGun)
+			obj.transform.parent = transform;
+			
+			//親要素からのオフセットは0
+			obj.transform.localPosition = Vector3.zero;
+			
+			//Gunからマウスクリックした地点にrayを飛ばす
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			//rayの方向を長さ1にして dirに代入
+			Vector3 dir = ray.direction.normalized;
+			
+			//生成したObjのrigidbodyを取得し、速度をdir方向に与える
+			obj.GetComponent<Rigidbody>().velocity = dir * 100.0f;
+		}
+
+	}
 }
