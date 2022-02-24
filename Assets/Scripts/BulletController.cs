d8c03429f41807ad568912f11e2b1f97284dfdf2
cf <hoge>
2022-02-24 12:37:36 +0900

    軌跡作成

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    //public float speed;
+    //public float destroyTime;
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
     }
+    /*
+     void Start () {
+        Destroy(gameObject, destroyTime);
+    }
+    
+    void Update () {
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+     */
 }
