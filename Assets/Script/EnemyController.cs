6610570952070824fe0a22b9f5bfe08a5362ae42
hogehoge <hogehoge@gmail.com>
2022-02-24 13:43:01 +0900

    Bulletが当たったらdamageアニメーションが起きてGameObjectが消える機能追加

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            //Debug.Log("Hit");
+
+            animator.SetTrigger("damage");
+
+            //Destroy(gameObject, 0.1f);
+        }
+        
+    }
+    public void OnDelete()
+    {
+        Destroy(gameObject, 0.1f);
+    }
+}
