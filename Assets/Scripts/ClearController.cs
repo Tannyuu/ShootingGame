b20f07d830a96c8cc1491559ac8834104c41f56f
sasaki <kunnrenn0909@gmail.com>
2022-02-28 09:30:51 +0900

    トロール追加仮のステージ２消去

---
+++ Assets/Scripts/ClearController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClearController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2秒後にReturnToTitleを呼び出す
+        Invoke("ReturnToTitle", 2.0f);
+    }
+    void ReturnToTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
